6b2792f07ade23d96e0cc42213f83c615f388e36
ammar ab <Ammar971@users.noreply.huggingface.co>
2023-11-04 18:36:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 import gradio as gr
 
-def greet(name, is_morning, temp):
-    salutatuin = "Gm" if is_morning else "Ge"
-    greeting = f"{salutatuin} {name}. It is {temp} degress today"
-    celsius = (temp - 32) * 5 /9
-    return greeting, round(celsius, 2)
+import tensorflow as tf
+import keras
+from matplotlib import pyplot as plt
+import numpy as np
 
+objects =  tf.keras.datasets.mnist
+(training_images, training_labels), (test_images, test_labels) = objects.load_data()
 
-demo = gr.Interface(fn=greet, inputs=["text", "checkbox", gr.Slider(0,50)], outputs=["text", "number"])
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
 
-demo.launch()
+from keras.layers import Flatten, Dense
+model = tf.keras.models.Sequential([Flatten(input_shape=(28,28)), 
+                                    Dense(256, activation='relu'),
+                                    Dense(256, activation='relu'),
+                                    Dense(128, activation='relu'), 
+                                    Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=10)
+
+test=test_images[0].reshape(-1,28,28)
+pred=model.predict(test)
+print(pred)
+
+iface = gr.Interface(predict_image, inputs="sketchpad", outputs="label")
+
+iface.launch(debug='True')
