e73e2faa865816bb64eb9099d13bcb8df2564238
ammar ab <Ammar971@users.noreply.huggingface.co>
2023-11-04 00:30:05 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+def greet(name, is_morning, temp):
+salutatuin = "Gm" if is_morning else "Ge"
+greeting = f"{salutatuin} {name}. It is {temp} degress today"
+celsius = (temp - 32) * 5 /9
+return greeting, round(celsius, 2)
+
+
+demo = gr.Interface(fn=greet, inputs=["text", "checkbox", gr.Slider(0,50)], outputs=["text", "number"])
+
+demo.launch()
