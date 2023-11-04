49da6b51756c407e087a059775ce066255d1a1bd
ammar ab <Ammar971@users.noreply.huggingface.co>
2023-11-04 18:41:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,13 @@ test=test_images[0].reshape(-1,28,28)
 pred=model.predict(test)
 print(pred)
 
+def predict_image(img):
+  img_3d=img.reshape(-1,28,28)
+  im_resize=img_3d/255.0
+  prediction=model.predict(im_resize)
+  pred=np.argmax(prediction)
+  return pred
+
 iface = gr.Interface(predict_image, inputs="sketchpad", outputs="label")
 
 iface.launch(debug='True')
